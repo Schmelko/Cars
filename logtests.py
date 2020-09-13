61cb5d7fe176cb86f184a7cf59030fc52bbc5364
Arnold Barna <arnold.barna@gmail.com>
2020-09-13 12:36:55 +0200

    Add Log tests test cases (test_task3, test_task4)

--- logtests.py
+++ logtests.py
@@ -1,5 +1,6 @@
 import unittest
 from log import Log
+from log_entry import LogEntry
 
 
 class TestSolution(unittest.TestCase):
@@ -15,6 +16,20 @@ class TestSolution(unittest.TestCase):
 
     def test_task2(self):
         self.assertEqual(self.log.findlastcar1(), ('30', '19:01', 'CEG307'))
+    
+    def test_task3(self):
+        expectedEntries = (
+            LogEntry('4	12:50	CEG303	561	5065	0'),
+            LogEntry('4	19:17	CEG308	552	27998	1')
+        )
+
+        result = self.log.findentriesbyday('4')
+
+        self.assertEqual(len(expectedEntries), 2)
+        self.assertEqual(expectedEntries[0], result[0])
+
+    def test_task4(self):
+        self.assertEqual(len(self.log.findCarsNotInGarageOnEndOfMonth()), 4)
 
 
 if __name__ == '__main__':
